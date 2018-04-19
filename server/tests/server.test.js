b80bddf940b910dd870ff36a2bf4d62785bc8685
Alexander Wesselburg <info@myportal.de>
2018-04-19 14:29:50 +0200

    =

---
+++ server/tests/server.test.js
@@ -0,0 +1,37 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => {
+    done();
+  })
+})
+
+describe('POST /todos', () => {
+  it('sould create a new todo', (done) => {
+    var text = 'Test todo text';
+    request(app)
+      .post('/todo')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length.toBe(3));
+          expect(todos[0].text.toBe(text))
+          done();
+        }).catch((e) => {
+          done(e)
+        })
+      })
+  })
+})
