cd60ec785590bf75af8096c16a3983fd7da4a649
Alexander Wesselburg <info@myportal.de>
2018-04-19 14:49:24 +0200

    =

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectId } = require('mongodb')
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -13,19 +14,32 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   })
-
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   })
-
 });
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send({ todos })
   }, (e) => {
+    res.status(400).send(e);
+  })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send(e);
+    }
+    res.send({ todo })
+  }).catch((e) => {
     res.status(400).send(e);
   })
 })
@@ -34,4 +48,4 @@ app.listen(3000, () => {
   console.log('Started on port 3000')
 })
 
-module.exports = {app};
+module.exports = { app };
